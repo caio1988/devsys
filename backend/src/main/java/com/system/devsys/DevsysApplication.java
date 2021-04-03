3f702f32fa6e9f6f851da0ade0f93c5d5bc62bbf
Caio <caio.orlandini@hotmail.com>
2021-04-02 23:00:44 -0300

    First commit

---
+++ backend/src/main/java/com/system/devsys/DevsysApplication.java
@@ -0,0 +1,13 @@
+package com.system.devsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsysApplication.class, args);
+	}
+
+}
